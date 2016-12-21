34cf102040d0e288daaa0de499233af73f37c614
Rasmus Wallin <rwa@rasmus.local>
2016-12-21 16:09:24 +0100

    Changed structure of statusObject

--- index.js
+++ index.js
@@ -4,32 +4,40 @@ const util = require('util');
 const files = require('./config/files');
 const fs = require('fs');
 const os = require('os');
+const disk = require('diskusage');
 
-var monitor = function() {
+var monitor = function(interval) {
 
   var _this = this;
+  this.interval = interval || 5000;
 
-  this.tick = function() {
-
-    var statusObject = {
-      hostname: os.hostname(),
-      loadavg: os.loadavg(),
-      uptime: os.uptime(),
-      freemem: os.freemem(),
-      totalmem: os.totalmem(),
-      cpus: os.cpus(),
-      platform: os.platform(),
-      type: os.type(),
-      arch: os.arch(),
-      release: os.release()
-    };
-    _this.emit('monitor', statusObject);
+  this.tick = function(curr, prev) {
+    disk.check('/', function(err, hdd) {
+      let statusObject = {
+        timestamp: new Date(),
+        uptime: os.uptime(),
+        memory: {
+          total: Math.floor(os.totalmem() / 1024 / 1024),
+          free: Math.floor(os.freemem()  / 1024 / 1024),
+          used: Math.floor((os.totalmem() - os.freemem()) / 1024 / 1024),
+          percentage: Math.floor(((os.totalmem() - os.freemem()) / os.totalmem()) * 100)
+        },
+        cpu: {
+          loadavg: os.loadavg()[0],
+          count: os.cpus().length,
+          percentage: Math.floor((os.loadavg()[0] / os.cpus().length) * 100)
+        },
+        hdd: {
+          total: Math.floor(hdd.total / 1024 / 1024),
+          free: Math.floor(hdd.free / 1024 / 1024),
+          used: Math.floor((hdd.total - hdd.free)  / 1024 / 1024),
+          percentage: Math.floor(((hdd.total - hdd.free) / hdd.total) * 100)
+        }
+      };
+      _this.emit('monitor', statusObject);
+    });
   };
-
-  files.map(function(file) {
-    fs.watchFile(file, _this.tick);
-  });
-
+  setInterval(_this.tick, _this.interval);
 };
 
 util.inherits(monitor, EventEmitter);
